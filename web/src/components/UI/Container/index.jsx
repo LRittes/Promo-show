7abf4f09c80141803bc16292f92c14175bae8533
lrittes <leandro_rittes@hotmail.com>
2021-08-29 15:45:00 -0300

    implement promotion search

---
+++ web/src/components/UI/Container/index.jsx
@@ -0,0 +1,9 @@
+import './styles.css'
+
+const UIContainer = ({children}) => (
+    <div className='ui-container'>
+        {children}
+    </div>
+)
+
+export default UIContainer;
--- web/src/pages/promotion/Search/index.jsx
+++ web/src/pages/promotion/Search/index.jsx
@@ -1,27 +1,13 @@
-import { useEffect, useState } from "react";
-import PromotionCard from "components/Promotion/Card";
-import axios from 'axios'
+import PromotionSearch from "components/Promotion/Search";
+import UIContainer from "components/UI/Container";
 
 const PagePromotionSearch = () => {
-  const [promotion, setPromotion] = useState([])
-
-  useEffect(() => {
-    axios.get('http://localhost:5000/promotions?_embed=comments')
-      .then(response => {
-        setPromotion(response.data)
-      })
-  }, [])
+  
 
   return (
-    <div 
-      style={{maxWidth: 800,
-              margin: '30px auto',
-              fontFamily: 'Arial', 
-      }}>
-        {promotion.map(promotion => (          
-          <PromotionCard promotion={promotion}/>    
-      ))}
-    </div>
+    <UIContainer>
+        <PromotionSearch/>
+    </UIContainer>
   )
 }
 
