63eec934dbe2a0354c52af90a541e4bbbef71584
lrittes <leandro_rittes@hotmail.com>
2021-09-06 14:16:08 -0300

    implement uimodal

--- web/src/components/Promotion/List/index.jsx
+++ web/src/components/Promotion/List/index.jsx
@@ -1,7 +1,13 @@
+import { useState } from 'react';
+
 import PromotionCard from '../Card';
+import UIModal from 'components/UI/Modal'
+
 import './styles.css'
 
 const PromotionList = ({ loading, error, promotions }) => {
+    const [promotionId, setPromotionId] = useState(null)
+
     if(error){
         return <div>Algo deu errado</div>
     }
@@ -17,8 +23,15 @@ const PromotionList = ({ loading, error, promotions }) => {
     return (
         <div className='promotion-list'>
             {promotions.map(promotion => ( 
-                <PromotionCard promotion={promotion} key={promotion.id}/>    
+                <PromotionCard 
+                    promotion={promotion} 
+                    key={promotion.id} 
+                    onClickComments={() => setPromotionId(promotion.id)}
+                />    
             ))}
+            <UIModal isOpen={Boolean(promotionId)} onClickClose={() => setPromotionId(null)}>
+                <h1>Deu boa</h1>
+            </UIModal>
         </div>
     )
 }
